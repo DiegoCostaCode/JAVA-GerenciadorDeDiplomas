12bba2d7cdf19f6f5c626d5e44b7fabf8716130b
Diego Costa <diegocosta.contato2021@gmail.com>
2024-10-08 09:50:08 -0300

    Iniciando projeto: setando depedencias e configurando conexão data-base

---
+++ src/main/java/cp2/java/gerenciador/de/diplomas/GerenciadorDeDiplomasApplication.java
@@ -0,0 +1,13 @@
+package cp2.java.gerenciador.de.diplomas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeDiplomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeDiplomasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cp2/java/gerenciador/de/diplomas/GerenciadorDeDiplomasApplicationTests.java
@@ -0,0 +1,13 @@
+package cp2.java.gerenciador.de.diplomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeDiplomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
